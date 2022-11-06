c982559056fd006067bbb21f6d3e7b20e683a42e
Kaku-U <gy291108750@icloud.com>
2022-11-06 22:56:36 +0800

    feat(00189): 00189-esay-awaited

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,11 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X > 
+    ? X extends Promise<unknown> 
+        ? MyAwaited<X> 
+        : X
+    : T
+
+
+// 知识点
+// 1. infer 只能在条件类型里使用
+// 2. 使用未知数 unknown < any
+// 3. 递归调用泛型
