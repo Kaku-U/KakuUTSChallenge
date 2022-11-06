b39516f1c8ca17623532171a4d4f7dd36e3bb412
Kaku-U <gy291108750@icloud.com>
2022-11-06 22:24:30 +0800

    feat(00043): 00043-esay-exclude

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,8 @@
-type Length<T> = any
+type Length<T extends readonly any[]> = T['length']
+
+
+// 知识点
+// 1.接受一个readonly 的数组
+// 2.T['length'] 返回类型里 'length' 属性的类型
+// 3. extends any[] 继承数组
+// 4.tuple 类型不同于普通的数组 因为是readonly 所以 tuple 的length 是一个固定的数字 而不是number 类型
--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,19 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T
+
+type t = 'a' | 'b' | 'c'  extends 'a' ? true : false
+
+type s = MyExclude<'a' | 'b' | 'c', 'a'>
+
+//js
+function MyExcludeFunc(t: any, u: any) {
+    let obj: any = {} 
+    for (const key in u) {
+        if(t[key]) {
+            obj[key] =  t[key]
+        }
+    }
+    return obj
+}
+
+//
+// 1. union extends union 会发生什么 
