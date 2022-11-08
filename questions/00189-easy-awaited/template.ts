01c88dc14fdf31afcaaf6d5ececb13c8b0c02d6d
Kaku-U <gy291108750@icloud.com>
2022-11-08 11:22:33 +0800

    feat(00268): 00268-easy-if

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,9 +1,8 @@
-type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X > 
-    ? X extends Promise<unknown> 
-        ? MyAwaited<X> 
-        : X
-    : T
-
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X >
+  ? X extends Promise<unknown>
+    ? MyAwaited<X>
+    : X
+  : T
 
 // 知识点
 // 1. infer 只能在条件类型里使用
--- questions/00268-easy-if/template.ts
+++ questions/00268-easy-if/template.ts
@@ -1,1 +1,12 @@
-type If<C, T, F> = any
+type If<C extends boolean, T, F> = C extends true ? T : F
+
+// js
+function IfFunc(c: boolean, t: any, f: any) {
+  if (typeof c === 'boolean')
+    return c ? t : f
+  else
+    return null
+}
+// 知识点
+// 1. C extends boolean 限制条件类型必须为bool类型
+// 2. C extends true 条件判断是否为T 类型 还是为 F 类型
